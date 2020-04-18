e921c4f72fcf81c5a5ba3665a4f3c95c504e2089
willian <54144699+willmont1982@users.noreply.github.com>
2020-04-18 11:42:03 -0300

    Update TeacherTest.php

--- Controle Escolar/tests/Domain/Teacher/TeacherTest.php
+++ Controle Escolar/tests/Domain/Teacher/TeacherTest.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * @Author: Leandro Henrique Reis <emtudo@gmail.com>
- * @Date:   2016-05-05 08:55:40
- * @Last Modified by:   Leandro Henrique Reis
- * @Last Modified time: 2016-06-04 19:51:10
- */
+
 
 namespace Domain\Teacher;
 
