6bb57ebf1cd7b6d884a853a900a0e249a6423b91
willian <54144699+willmont1982@users.noreply.github.com>
2020-04-18 11:45:33 -0300

    Update BilletAssignorControllerTest.php

--- Controle Escolar/tests/Domain/Billet/Http/BilletAssignorControllerTest.php
+++ Controle Escolar/tests/Domain/Billet/Http/BilletAssignorControllerTest.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * @Author: Leandro Henrique Reis <emtudo@gmail.com>
- * @Date:   2016-05-13 07:59:59
- * @Last Modified by:   Leandro Henrique Reis
- * @Last Modified time: 2016-06-04 19:59:06
- */
+
 
 namespace Domain\Billet\Http;
 
