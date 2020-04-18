3e346e4849ae9984bbae4a22c0d2e2d68e4dda56
willian <54144699+willmont1982@users.noreply.github.com>
2020-04-18 11:52:29 -0300

    Update MatterControllerTest.php

--- Controle Escolar/tests/Domain/Matter/Http/MatterControllerTest.php
+++ Controle Escolar/tests/Domain/Matter/Http/MatterControllerTest.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * @Author: Leandro Henrique Reis <emtudo@gmail.com>
- * @Date:   2016-05-13 08:02:56
- * @Last Modified by:   Leandro Henrique Reis
- * @Last Modified time: 2016-06-04 19:58:58
- */
+
 
 namespace Domain\Matter\Http;
 
