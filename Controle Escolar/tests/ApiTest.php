c4cff5452f291e094f2ae422b3bb163ba9ab038d
willian <54144699+willmont1982@users.noreply.github.com>
2020-04-18 11:40:58 -0300

    Update ApiTest.php

--- Controle Escolar/tests/ApiTest.php
+++ Controle Escolar/tests/ApiTest.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * @Author: Leandro Henrique Reis <emtudo@gmail.com>
- * @Date:   2016-04-24 06:50:43
- * @Last Modified by:   Leandro Henrique Reis
- * @Last Modified time: 2016-06-04 19:59:50
- */
+
 class ApiTest extends TestCase
 {
     /**
