2da3449bce9a20fcca24c818d997c13d30d01848
willian <54144699+willmont1982@users.noreply.github.com>
2020-04-18 11:47:33 -0300

    Update ConfigTest.php

--- Controle Escolar/tests/Domain/Config/ConfigTest.php
+++ Controle Escolar/tests/Domain/Config/ConfigTest.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * @Author: Leandro Henrique Reis <emtudo@gmail.com>
- * @Date:   2016-05-05 08:41:33
- * @Last Modified by:   Leandro Henrique Reis
- * @Last Modified time: 2016-06-04 19:50:52
- */
 
 namespace Domain\Config;
 
