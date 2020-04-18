527834447aa0eaad07fa919ef3b1ad8b8f6b40c0
willian <54144699+willmont1982@users.noreply.github.com>
2020-04-18 11:46:16 -0300

    Update ClassroomTest.php

--- Controle Escolar/tests/Domain/Classroom/ClassroomTest.php
+++ Controle Escolar/tests/Domain/Classroom/ClassroomTest.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * @Author: Leandro Henrique Reis <emtudo@gmail.com>
- * @Date:   2016-05-06 22:00:25
- * @Last Modified by:   Leandro Henrique Reis
- * @Last Modified time: 2016-06-04 19:50:49
- */
 
 namespace Domain\Classroom;
 
