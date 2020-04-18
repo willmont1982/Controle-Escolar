9852c872677f8a35d36fc7c94a395dacbb41d700
willian <54144699+willmont1982@users.noreply.github.com>
2020-04-18 11:53:57 -0300

    Update ScheduleControllerTest.php

--- Controle Escolar/tests/Domain/Schedule/Http/ScheduleControllerTest.php
+++ Controle Escolar/tests/Domain/Schedule/Http/ScheduleControllerTest.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * @Author: Leandro Henrique Reis <emtudo@gmail.com>
- * @Date:   2016-05-13 08:03:24
- * @Last Modified by:   Leandro Henrique Reis
- * @Last Modified time: 2016-06-04 19:58:56
- */
+
 
 namespace Domain\Schedule\Http;
 
