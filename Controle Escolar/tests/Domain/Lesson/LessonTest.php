94b38c289ce3a57c84043eda2179e200e0e4cd81
willian <54144699+willmont1982@users.noreply.github.com>
2020-04-18 11:50:48 -0300

    Update LessonTest.php

--- Controle Escolar/tests/Domain/Lesson/LessonTest.php
+++ Controle Escolar/tests/Domain/Lesson/LessonTest.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * @Author: Leandro Henrique Reis <emtudo@gmail.com>
- * @Date:   2016-05-06 22:02:58
- * @Last Modified by:   Leandro Henrique Reis
- * @Last Modified time: 2016-06-04 19:50:58
- */
+
 
 namespace Domain\Lesson;
 
